518b50e17a642f05c30f2965624c86a670fde1d9
Ashley Hsiao <aiyihsiao@gmail.com>
2016-07-28 16:57:11 -0700

    Display the latest 5 poll questions in the system for index view

--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
