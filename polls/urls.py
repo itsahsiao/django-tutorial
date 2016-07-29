d2449dbf2296080e2854d464055be7e28d57f61d
Ashley Hsiao <aiyihsiao@gmail.com>
2016-07-28 23:20:38 -0700

    Add namespace

--- polls/urls.py
+++ polls/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # added the word 'specifics'
-    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
