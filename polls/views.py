06a750be769b6a7135da81b5a795ad8f263bd11d
Ashley Hsiao <aiyihsiao@gmail.com>
2016-07-29 00:04:11 -0700

    Avoid race conditions using F()

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.db.models import F
 
 from .models import Choice, Question
 
@@ -45,7 +46,7 @@ def vote(request, question_id):
         })
 
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
