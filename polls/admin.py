ef918c11eb822ad0582c5288d4bf07982abe3829
Ashley Hsiao <aiyihsiao@gmail.com>
2016-07-31 17:48:40 -0700

    Reorder fields on edit form in admin for Questions

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Question
 
-admin.site.register(Question)
+
+# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
